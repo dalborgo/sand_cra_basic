cac1a2cf0c7ff2d7e0ff9e6d18e7dd3088ef0e27
dalborgo <marco@dalborgo.it>
2021-03-01 17:46:28 +0100

    import

--- src/base/page.js
+++ src/base/page.js
@@ -1,56 +1,50 @@
 import React from 'react'
 import Page from 'src/components/Page'
-import { Box, makeStyles } from '@material-ui/core'
+import { makeStyles, Paper } from '@material-ui/core'
 import { FormattedMessage, useIntl } from 'react-intl'
 import { messages } from 'src/translations/messages'
 import StandardHeader from 'src/components/StandardHeader'
+import { StandardBreadcrumb } from 'src/components/StandardBreadcrumb'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.dark,
+import DivContentWrapper from 'src/components/DivContentWrapper'
+
+const useStyles = makeStyles(theme => ({
+  paper: {
     height: '100%',
-    display: 'flex',
-    overflow: 'hidden',
-    flexDirection: 'column',
   },
-  content: {
-    flexGrow: 1,
-    flexShrink: 1,
-    display: 'flex',
-    overflowY: 'hidden',
-    overflowX: 'auto',
-  },
-  innerFirst: {
-    display: 'flex',
+  container: {
     padding: theme.spacing(2),
-    paddingTop: theme.spacing(1),
-    width: '100%',
-    [theme.breakpoints.down('xs')]: {
-      padding: 0,
+    [theme.breakpoints.down('sm')]: {// mobile
+      padding: theme.spacing(0, 2),
     },
   },
 }))
 
-const ClosingDay = () => {
+const Import = () => {
   const classes = useStyles()
   const intl = useIntl()
   return (
     <Page
-      className={classes.root}
-      title={intl.formatMessage(messages['menu_closing_day'])}
+      title={intl.formatMessage(messages['menu_import'])}
     >
-      <Box p={3} pb={2}>
-        <StandardHeader>
-          <FormattedMessage defaultMessage="Chiusure di giornata" id="reports.closing_day.header_title"/>
+      <div className={classes.container}>
+        <StandardHeader
+          breadcrumb={
+            <StandardBreadcrumb
+              crumbs={[{ to: '/app', name: 'DashBoard' }, { name: intl.formatMessage(messages['sub_management']) }]}
+            />
+          }
+        >
+          <FormattedMessage defaultMessage="Import" id="management.import.header_title"/>
         </StandardHeader>
-      </Box>
-      <div className={classes.content}>
-        <div className={classes.innerFirst}>
-          Running Tables
-        </div>
       </div>
+      <DivContentWrapper>
+        <Paper className={classes.paper}>
+          Prova
+        </Paper>
+      </DivContentWrapper>
     </Page>
   )
 }
 
-export default ClosingDay
+export default Import
