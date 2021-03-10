eac52107e4e233b1fd5f907aa46c6767f21c4dbb
dalborgo <Montebaldo1>
2021-03-11 00:02:26 +0100

    crash

--- src/base/page.js
+++ src/base/page.js
@@ -35,7 +35,7 @@ const Import = () => {
             />
           }
         >
-          <FormattedMessage defaultMessage="Import" id="management.import.header_title"/>
+          <FormattedMessage defaultMessage="Import/export" id="management.import.header_title"/>
         </StandardHeader>
       </div>
       <DivContentWrapper>
